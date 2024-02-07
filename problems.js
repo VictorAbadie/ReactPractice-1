0dd335b429afce7f7a6057a0d998a8fd031b127b
Victor Abadie <victor_abadie@aol.com>
2024-02-07 12:07:16 -0600

    Final Solution to Problem #2

--- problems.js
+++ problems.js
@@ -19,11 +19,12 @@
 // Let’s start with the tables that many of us had to memorize in school. Can you print a table that contains all the answers to the multiplication tables from 1 through 10?
 // Like Challenge #1, can you create an efficient solution that you could easily expand should you need the 12 times table?
 
-let result = " x\t 1\t 2\t 3\t 4\t 5\t 6\t 7\t 8\t 9\t10 "
-for (let i = 1; i <= 10; i++) {
-    result += `\n${i < 10 ? ' ' + i : i}`
-    for (let j = 1; j <= 10; j++) {
-        result += `\t${i * j >= 10 ? i*j : ' ' + i*j}`
+// let result = " x\t 1\t 2\t 3\t 4\t 5\t 6\t 7\t 8\t 9\t10 "
+let result = "x\t1\t2\t3\t4\t5\t6\t7\t8\t9\t10\t11\t12"
+for (let i = 1; i <= 12; i++) {
+    result += `\n${i < 12 ? '' + i : i}`
+    for (let j = 1; j <= 12; j++) {
+        result += `\t${i * j >= 12 ? i*j : '' + i*j}`
     }
 }
 console.log(result)
